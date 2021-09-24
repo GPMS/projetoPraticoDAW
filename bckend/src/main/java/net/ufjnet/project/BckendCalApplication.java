c03bcfb9d723b646705d0aa5363432a073cbe8bc
GPMS <gab.portela@hotmail.com>
2021-09-23 23:12:39 -0300

    Back-End: Remover usuários pre cadastrados

--- bckend/src/main/java/net/ufjnet/project/BckendCalApplication.java
+++ bckend/src/main/java/net/ufjnet/project/BckendCalApplication.java
@@ -16,19 +16,15 @@ import net.ufjnet.project.repositories.DAO;
 @ComponentScan
 @SpringBootApplication
 public class BckendCalApplication implements CommandLineRunner {
-	
+
 	@Autowired
 	private DAO userDAO;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(BckendCalApplication.class, args);
 	}
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		Model u1 = new Model(1, "Gabriel", "gabriel@cal");
-		Model u2 = new Model(2, "Gabrieli", "gabrieli@cal");
-		Model u3 = new Model(3, "Roberto", "roberto@cal");
-		userDAO.saveAll(Arrays.asList(u1, u2, u3));
 	}
 }
